06c9c06d6ac4527805bf16865f3aaf95ec45095f
DanielvdC <63288710+DanielvdC@users.noreply.github.com>
2020-05-29 14:12:53 +0200

    Add files via upload

--- LISclassification.R
+++ LISclassification.R
@@ -17,7 +17,7 @@ library(data.table)
 
 # Variable and path definition -------------------------------------------------
 
-pre_files <- 'INPUT_FILE'
+pre_files <- 'INPUT_FILES'
 N400_c <- c('Cz', 'CP1', 'CP2', 'P3', 'Pz', 'P4', 'POz')
 N400_t <- c(250, 600)
 N400_c_txt <- paste0(N400_c, collapse = ", ")
@@ -53,16 +53,16 @@ eeg_data_ica <- map_dfr(
 # Merge preprocessed files together in 1 dataframe -----------------------------
 # Without subject numbering
 c <- 0
-for (file in list.files('pca1/')){
+for (file in list.files('PREPROCESSED_FILES/')){
   if (c == 0){
-    RDSfile <- paste0('pca1/', file)
+    RDSfile <- paste0('PREPROCESSED_FILES/', file)
     df <- readRDS(RDSfile)
     df$.sample <- as.factor(df$.sample)
     df <- as.data.frame(df)
     c = c + 1
   }
   else{
-    RDSfile <- paste0('pca1/', file)
+    RDSfile <- paste0('PREPROCESSED_FILES/', file)
     df1 <- readRDS(RDSfile)
     df1$.sample <- as.factor(df1$.sample)
     df1 <- as.data.frame(df1)
